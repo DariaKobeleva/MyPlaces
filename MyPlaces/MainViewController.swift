2f3353adafefa257790719ca65b89bfc730eba93
KDaria <dkdva@mail.ru>
2023-08-08 16:25:17 +0200

    Add new controller

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -9,12 +9,9 @@ import UIKit
 
 class MainViewController: UITableViewController {
     
-    let restaurantNames = [
-    "Kafeterija", "Bela Reka", "Butique", "Stories",
-    "TT_Bistro", "HookahHouse", "Heritage Rooftop Gastro Bar",
-    "City Garden", "Sinnerman Jazz Club",
-    "Caruso", "SkyLounge", "THE VIEW", "Kafeterija Secer"
-    ]
+
+    
+    let places = Place.getPlaces()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,24 +23,22 @@ class MainViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restaurantNames.count
+        return places.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        cell.nameLabel.text = restaurantNames[indexPath.row]
-        cell.imageOfPlace.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.nameLabel.text = places[indexPath.row].name
+        cell.locationLabel.text = places[indexPath.row].location
+        cell.typeLabel.text = places[indexPath.row].type
+        cell.imageOfPlace.image = UIImage(named: places[indexPath.row].image)
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace.clipsToBounds = true
         
         return cell
     }
-    // MARK: - Table view delegate
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
-    }
+     
 
     /*
     // MARK: - Navigation
@@ -54,5 +49,7 @@ class MainViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func canselAction(_ segue: UIStoryboardSegue) {}
 
 }
---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,35 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Дарья Кобелева on 04.08.2023.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [
+        "Kafeterija", "Bela Reka", "Butique", "Stories",
+        "TT_Bistro", "HookahHouse", "Heritage Rooftop Gastro Bar",
+        "City Garden", "Sinnerman Jazz Club",
+        "Caruso", "SkyLounge", "THE VIEW", "Kafeterija Secer"
+        ]
+    
+   static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place , location: "Belgrade", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+}
+
